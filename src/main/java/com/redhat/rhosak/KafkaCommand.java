010cc7fafbd1c172f491204a3c91098b9e0e9357
Maksym Severinovskyi <maksym.severinovskyi@eleks.com>
2022-06-14 15:22:50 +0300

    Move Kafka Command and Kafka Acl Command classes to separate files

---
+++ src/main/java/com/redhat/rhosak/KafkaCommand.java
@@ -0,0 +1,111 @@
+package com.redhat.rhosak;
+
+import com.openshift.cloud.api.kas.DefaultApi;
+import com.openshift.cloud.api.kas.invoker.ApiException;
+import com.openshift.cloud.api.kas.models.KafkaRequest;
+import com.openshift.cloud.api.kas.models.KafkaRequestPayload;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.Objects;
+import java.util.concurrent.Callable;
+
+@Command(name = "kafka", mixinStandardHelpOptions = true, description = "Create, view and manage your Kafka instances",
+        subcommands = {KafkaCreateCommand.class, KafkaListCommand.class,
+                KafkaTopicCommand.class, KafkaDeleteCommand.class, KafkaAclCommand.class})
+public class KafkaCommand implements Callable<Integer> {
+
+    @Override
+    public Integer call() {
+        return 0;
+    }
+}
+
+@Command(name = "create", mixinStandardHelpOptions = true, description = "Create kafka instance")
+class KafkaCreateCommand implements Callable<Integer> {
+
+    private final DefaultApi managementAPI;
+
+    public KafkaCreateCommand() {
+        this.managementAPI =
+                new DefaultApi(
+                        KafkaManagementClient.getKafkaManagementAPIClient()
+                );
+    }
+
+    @CommandLine.Option(names = "--name", paramLabel = "string", description = "Name of the kafka instance")
+    String instanceName;
+
+    @Override
+    public Integer call() {
+        if (Objects.nonNull(instanceName)) {
+            System.out.println(createInstance(instanceName));
+        }
+
+        return 0;
+    }
+
+    private KafkaRequest createInstance(String name) {
+        KafkaRequestPayload kafkaRequestPayload = new KafkaRequestPayload();
+        kafkaRequestPayload.setName(name);
+
+        try {
+            return managementAPI.createKafka(true, kafkaRequestPayload);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling DefaultApi#createKafka");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
+
+@Command(name = "list", mixinStandardHelpOptions = true, description = "List all kafka instances")
+class KafkaListCommand implements Callable<Integer> {
+
+    private final DefaultApi managementAPI;
+
+    public KafkaListCommand() {
+        this.managementAPI =
+                new DefaultApi(
+                        KafkaManagementClient.getKafkaManagementAPIClient()
+                );
+    }
+
+    @Override
+    public Integer call() {
+        try {
+            System.out.println(managementAPI.getKafkas(null, null, null, null).getItems());
+        } catch (ApiException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return 0;
+    }
+}
+
+@Command(name = "delete", mixinStandardHelpOptions = true, description = "Delete a kafka instance")
+class KafkaDeleteCommand implements Callable<Integer> {
+
+    private final DefaultApi managementAPI;
+
+    @CommandLine.Option(names = "--id", paramLabel = "string", required = true, description = "Unique ID of the Kafka instance you want to delete")
+    String kafkaId;
+
+    public KafkaDeleteCommand() {
+        this.managementAPI =
+                new DefaultApi(
+                        KafkaManagementClient.getKafkaManagementAPIClient()
+                );
+    }
+
+    @Override
+    public Integer call() {
+        try {
+            System.out.println(managementAPI.deleteKafkaById(kafkaId, true));
+        } catch (ApiException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return 0;
+    }
+}
--- src/main/java/com/redhat/rhosak/Rhosak.java
+++ src/main/java/com/redhat/rhosak/Rhosak.java
@@ -57,118 +57,118 @@ public class Rhosak implements Callable<Integer> {
     }
 }
 
-@Command(name = "kafka", mixinStandardHelpOptions = true, description = "Create, view and manage your Kafka instances",
-        subcommands = {KafkaCreateCommand.class, KafkaListCommand.class,
-                KafkaTopicCommand.class, KafkaDeleteCommand.class, KafkaAclCommand.class})
-class KafkaCommand implements Callable<Integer> {
+//@Command(name = "kafka", mixinStandardHelpOptions = true, description = "Create, view and manage your Kafka instances",
+//        subcommands = {KafkaCreateCommand.class, KafkaListCommand.class,
+//                KafkaTopicCommand.class, KafkaDeleteCommand.class, KafkaAclCommand.class})
+//class KafkaCommand implements Callable<Integer> {
+//
+//    @Override
+//    public Integer call() {
+//        return 0;
+//    }
+//}
+//
+//@Command(name = "create", mixinStandardHelpOptions = true, description = "Create kafka instance")
+//class KafkaCreateCommand implements Callable<Integer> {
+//
+//    private final DefaultApi managementAPI;
+//
+//    public KafkaCreateCommand() {
+//        this.managementAPI =
+//                new DefaultApi(
+//                        KafkaManagementClient.getKafkaManagementAPIClient()
+//                );
+//    }
+//
+//    @CommandLine.Option(names = "--name", paramLabel = "string", description = "Name of the kafka instance")
+//    String instanceName;
+//
+//    @Override
+//    public Integer call() {
+//        if (Objects.nonNull(instanceName)) {
+//            System.out.println(createInstance(instanceName));
+//        }
+//
+//        return 0;
+//    }
+//
+//    private KafkaRequest createInstance(String name) {
+//        KafkaRequestPayload kafkaRequestPayload = new KafkaRequestPayload();
+//        kafkaRequestPayload.setName(name);
+//
+//        try {
+//            return managementAPI.createKafka(true, kafkaRequestPayload);
+//        } catch (ApiException e) {
+//            System.err.println("Exception when calling DefaultApi#createKafka");
+//            System.err.println("Status code: " + e.getCode());
+//            System.err.println("Reason: " + e.getResponseBody());
+//            System.err.println("Response headers: " + e.getResponseHeaders());
+//            throw new RuntimeException(e.getMessage());
+//        }
+//    }
+//}
+//
+//@Command(name = "list", mixinStandardHelpOptions = true, description = "List all kafka instances")
+//class KafkaListCommand implements Callable<Integer> {
+//
+//    private final DefaultApi managementAPI;
+//
+//    public KafkaListCommand() {
+//        this.managementAPI =
+//                new DefaultApi(
+//                        KafkaManagementClient.getKafkaManagementAPIClient()
+//                );
+//    }
+//
+//    @Override
+//    public Integer call() {
+//        try {
+//            System.out.println(managementAPI.getKafkas(null, null, null, null).getItems());
+//        } catch (ApiException e) {
+//            throw new RuntimeException(e.getMessage());
+//        }
+//        return 0;
+//    }
+//}
+//
+//@Command(name = "delete", mixinStandardHelpOptions = true, description = "Delete a kafka instance")
+//class KafkaDeleteCommand implements Callable<Integer> {
+//
+//    private final DefaultApi managementAPI;
+//
+//    @CommandLine.Option(names = "--id", paramLabel = "string", required = true, description = "Unique ID of the Kafka instance you want to delete")
+//    String kafkaId;
+//
+//    public KafkaDeleteCommand() {
+//        this.managementAPI =
+//                new DefaultApi(
+//                        KafkaManagementClient.getKafkaManagementAPIClient()
+//                );
+//    }
+//
+//    @Override
+//    public Integer call() {
+//        try {
+//            System.out.println(managementAPI.deleteKafkaById(kafkaId, true));
+//        } catch (ApiException e) {
+//            throw new RuntimeException(e.getMessage());
+//        }
+//        return 0;
+//    }
+//}
 
-    @Override
-    public Integer call() {
-        return 0;
-    }
-}
-
-@Command(name = "create", mixinStandardHelpOptions = true, description = "Create kafka instance")
-class KafkaCreateCommand implements Callable<Integer> {
-
-    private final DefaultApi managementAPI;
-
-    public KafkaCreateCommand() {
-        this.managementAPI =
-                new DefaultApi(
-                        KafkaManagementClient.getKafkaManagementAPIClient()
-                );
-    }
-
-    @CommandLine.Option(names = "--name", paramLabel = "string", description = "Name of the kafka instance")
-    String instanceName;
-
-    @Override
-    public Integer call() {
-        if (Objects.nonNull(instanceName)) {
-            System.out.println(createInstance(instanceName));
-        }
-
-        return 0;
-    }
-
-    private KafkaRequest createInstance(String name) {
-        KafkaRequestPayload kafkaRequestPayload = new KafkaRequestPayload();
-        kafkaRequestPayload.setName(name);
-
-        try {
-            return managementAPI.createKafka(true, kafkaRequestPayload);
-        } catch (ApiException e) {
-            System.err.println("Exception when calling DefaultApi#createKafka");
-            System.err.println("Status code: " + e.getCode());
-            System.err.println("Reason: " + e.getResponseBody());
-            System.err.println("Response headers: " + e.getResponseHeaders());
-            throw new RuntimeException(e.getMessage());
-        }
-    }
-}
-
-@Command(name = "list", mixinStandardHelpOptions = true, description = "List all kafka instances")
-class KafkaListCommand implements Callable<Integer> {
-
-    private final DefaultApi managementAPI;
-
-    public KafkaListCommand() {
-        this.managementAPI =
-                new DefaultApi(
-                        KafkaManagementClient.getKafkaManagementAPIClient()
-                );
-    }
-
-    @Override
-    public Integer call() {
-        try {
-            System.out.println(managementAPI.getKafkas(null, null, null, null).getItems());
-        } catch (ApiException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        return 0;
-    }
-}
-
-@Command(name = "delete", mixinStandardHelpOptions = true, description = "Delete a kafka instance")
-class KafkaDeleteCommand implements Callable<Integer> {
-
-    private final DefaultApi managementAPI;
-
-    @CommandLine.Option(names = "--id", paramLabel = "string", required = true, description = "Unique ID of the Kafka instance you want to delete")
-    String kafkaId;
-
-    public KafkaDeleteCommand() {
-        this.managementAPI =
-                new DefaultApi(
-                        KafkaManagementClient.getKafkaManagementAPIClient()
-                );
-    }
-
-    @Override
-    public Integer call() {
-        try {
-            System.out.println(managementAPI.deleteKafkaById(kafkaId, true));
-        } catch (ApiException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        return 0;
-    }
-}
-
-@Command(name = "acl", mixinStandardHelpOptions = true,
-        description = "Manage Kafka ACLs for users and service accounts",
-        subcommands = KafkaAclCreateCommand.class)
-class KafkaAclCommand implements Callable<Integer> {
-
-    public KafkaAclCommand() {}
-
-    @Override
-    public Integer call() {
-        return 0;
-    }
-}
+//@Command(name = "acl", mixinStandardHelpOptions = true,
+//        description = "Manage Kafka ACLs for users and service accounts",
+//        subcommands = KafkaAclCreateCommand.class)
+//class KafkaAclCommand implements Callable<Integer> {
+//
+//    public KafkaAclCommand() {}
+//
+//    @Override
+//    public Integer call() {
+//        return 0;
+//    }
+//}
 
 class CustomCommand {
     private final ApiClient apiManagementClient;
@@ -189,54 +189,54 @@ class CustomCommand {
 
 }
 
-@Command(name = "create", mixinStandardHelpOptions = true, description = "Create a Kafka ACL")
-class KafkaAclCreateCommand extends CustomCommand implements Callable<Integer> {
-
-    @CommandLine.Option(names = "--operation", paramLabel = "string", required = true, description = "Set the ACL operation. Choose from: \"all\", \"alter\", \"alter-configs\", \"create\", \"delete\", \"describe\", \"describe-configs\", \"read\", \"write\"")
-    String operation;
-
-    @CommandLine.Option(names = "--permission", paramLabel = "string", required = true, description = "Set the ACL permission. Choose from: \"allow\", \"deny\"")
-    String permission;
-
-    @CommandLine.Option(names = "--service-account", paramLabel = "string", description = "Service account client ID used as principal for this operation")
-    String serviceAccountClientID;
-
-    @CommandLine.Option(names = "--topic", paramLabel = "string", description = "Set the topic resource")
-    String topicResource;
-
-    public KafkaAclCreateCommand() {}
-
-    @Override
-    public Integer call() {
-        com.openshift.cloud.api.kas.auth.invoker.ApiClient defaultClient =
-                com.openshift.cloud.api.kas.auth.invoker.Configuration.getDefaultApiClient();
-        defaultClient.setBasePath("http://localhost");
-
-        // Configure OAuth2 access token for authorization: Bearer
-        OAuth bearer = (OAuth) defaultClient.getAuthentication("Bearer");
-        bearer.setAccessToken(KafkaManagementClient.getBearerToken());
-
-        AclsApi aclsApi = new AclsApi(defaultClient);
-        aclsApi.getApiClient().setBasePath(getServerUrl());
-
-        AclBinding aclBinding =  new AclBinding();
-        AclPermissionType permType = AclPermissionType.ALLOW;
-
-        aclBinding.setPermission(permType);                // Permission
-        aclBinding.setResourceName(topicResource);         // Topic instance name
-        aclBinding.setResourceType(AclResourceType.TOPIC); // Topic
-        aclBinding.setPrincipal("User:*");   // User:...
-        aclBinding.setOperation(AclOperation.ALL);         // Operation
-        aclBinding.setPatternType(AclPatternType.LITERAL); // Pattern Type
-        try {
-            aclsApi.createAcl(aclBinding);
-
-        } catch (com.openshift.cloud.api.kas.auth.invoker.ApiException e) {
-            e.printStackTrace();
-        }
-        return 0;
-    }
-}
+//@Command(name = "create", mixinStandardHelpOptions = true, description = "Create a Kafka ACL")
+//class KafkaAclCreateCommand extends CustomCommand implements Callable<Integer> {
+//
+//    @CommandLine.Option(names = "--operation", paramLabel = "string", required = true, description = "Set the ACL operation. Choose from: \"all\", \"alter\", \"alter-configs\", \"create\", \"delete\", \"describe\", \"describe-configs\", \"read\", \"write\"")
+//    String operation;
+//
+//    @CommandLine.Option(names = "--permission", paramLabel = "string", required = true, description = "Set the ACL permission. Choose from: \"allow\", \"deny\"")
+//    String permission;
+//
+//    @CommandLine.Option(names = "--service-account", paramLabel = "string", description = "Service account client ID used as principal for this operation")
+//    String serviceAccountClientID;
+//
+//    @CommandLine.Option(names = "--topic", paramLabel = "string", description = "Set the topic resource")
+//    String topicResource;
+//
+//    public KafkaAclCreateCommand() {}
+//
+//    @Override
+//    public Integer call() {
+//        com.openshift.cloud.api.kas.auth.invoker.ApiClient defaultClient =
+//                com.openshift.cloud.api.kas.auth.invoker.Configuration.getDefaultApiClient();
+//        defaultClient.setBasePath("http://localhost");
+//
+//        // Configure OAuth2 access token for authorization: Bearer
+//        OAuth bearer = (OAuth) defaultClient.getAuthentication("Bearer");
+//        bearer.setAccessToken(KafkaManagementClient.getBearerToken());
+//
+//        AclsApi aclsApi = new AclsApi(defaultClient);
+//        aclsApi.getApiClient().setBasePath(getServerUrl());
+//
+//        AclBinding aclBinding =  new AclBinding();
+//        AclPermissionType permType = AclPermissionType.ALLOW;
+//
+//        aclBinding.setPermission(permType);                // Permission
+//        aclBinding.setResourceName(topicResource);         // Topic instance name
+//        aclBinding.setResourceType(AclResourceType.TOPIC); // Topic
+//        aclBinding.setPrincipal("User:*");   // User:...
+//        aclBinding.setOperation(AclOperation.ALL);         // Operation
+//        aclBinding.setPatternType(AclPatternType.LITERAL); // Pattern Type
+//        try {
+//            aclsApi.createAcl(aclBinding);
+//
+//        } catch (com.openshift.cloud.api.kas.auth.invoker.ApiException e) {
+//            e.printStackTrace();
+//        }
+//        return 0;
+//    }
+//}
 
 //@Command(name = "topic", mixinStandardHelpOptions = true, description = "Create, describe, update, list, and delete topics",
 //        subcommands = KafkaTopicCreateCommand.class)
@@ -630,9 +630,9 @@ class RhoasTokens {
     }
 }
 
-class Files {
+//class Files {
 //    public static final String KEYCLOAK_CONFIG_FILE = "keycloak.json";
 //    public static final String DEFAULT_CREDENTIALS_FILENAME = "credentials.json";
 //    public static final String SA_FILE_NAME = System.getProperty("user.dir") + File.separator + "rhosak-sa";
 //    public static final String RHOSAK_API_CREDS_FILE_NAME = System.getProperty("user.dir") + File.separator + "rhosak-api-creds";
-}
+//}
