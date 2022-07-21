ac2e573ebf9bcdd54f3bd62545f1a4c69c08833f
Maksym Severinovskyi <maksym.severinovskyi@eleks.com>
2022-07-21 16:16:51 +0300

    Write artifact to file

--- src/main/java/com/redhat/rhosak/service/registry/artifact/ServiceRegistryArtifactDownloadCommand.java
+++ src/main/java/com/redhat/rhosak/service/registry/artifact/ServiceRegistryArtifactDownloadCommand.java
@@ -1,22 +1,38 @@
 package com.redhat.rhosak.service.registry.artifact;
 
 import com.openshift.cloud.api.kas.auth.invoker.ApiClient;
+import com.openshift.cloud.api.kas.auth.invoker.ApiException;
 import com.redhat.rhosak.CustomCommand;
 import com.redhat.rhosak.KafkaInstanceClient;
 import picocli.CommandLine;
 
+import javax.ws.rs.core.GenericType;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.Callable;
+
+import static com.redhat.rhosak.KafkaManagementClient.API_CLIENT_BASE_PATH;
 
 @CommandLine.Command(name = "download", mixinStandardHelpOptions = true, description = "Download artifacts from Service Registry")
 public class ServiceRegistryArtifactDownloadCommand extends CustomCommand implements Callable<Integer> {
 
     private final ApiClient apiInstanceClient;
 
-    @CommandLine.Option(names = "--file", paramLabel = "string", description = "File location of the artifact")
-    String fileName;
+    @CommandLine.Option(names = "--content-id", paramLabel = "int", description = "ContentId of the artifact")
+    String contentId;
 
-    @CommandLine.Option(names = "--type", paramLabel = "string", description = "Type of artifact")
-    String artifactType;
+    @CommandLine.Option(names = "--global-id", paramLabel = "int", description = "Global ID of the artifact")
+    String globalId;
+
+    @CommandLine.Option(names = "--output-file", paramLabel = "string", description = "File location of the artifact")
+    String outputFile;
 
     public ServiceRegistryArtifactDownloadCommand() {
         this.apiInstanceClient = KafkaInstanceClient.getKafkaInstanceAPIClient();
@@ -24,58 +40,79 @@ public class ServiceRegistryArtifactDownloadCommand extends CustomCommand implem
 
     @Override
     public Integer call() {
+        final String ARTIFACT_DOWNLOAD_URL = "/apis/registry/v2/ids";
 
-//        String body;
-//        try {
-//            if (fileName == null) {
-//                // read the artifact from System.in
-//                body = new String(System.in.readAllBytes());
-//            } else {
-//                // read the artifact from file
-//                body = new String(Files.readAllBytes(Paths.get(fileName)));
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        final String artifactTypeRequestHeader = "X-Registry-Artifacttype";
-//        Map<String, String> headerParams = new HashMap<>();
-//        if (artifactType != null) {
-//            // Add artifact type Header
-//            headerParams.put(artifactTypeRequestHeader, artifactType);
-//        }
-//        ServiceRegistryDTO serviceRegistry = ServiceRegistryUtils.getServiceRegistry();
-//        String registryUrl = serviceRegistry.getRegistryUrl();
-//        apiInstanceClient.setBasePath(registryUrl);
-//        final String ARTIFACT_CREATE_URL = "/apis/registry/v2/groups/default/artifacts";
-//
-//        try {
-//            Map<String, Object> artifactsMap = apiInstanceClient.invokeAPI(
-//                    ARTIFACT_CREATE_URL, "POST", null, body,
-//                    headerParams, new HashMap<>(), new HashMap<>(), ACCEPT_APPLICATION_JSON, CONTENT_TYPE_APPLICATION_JSON,
-//                    new String[]{"Bearer"}, new GenericType<>() {}
-//            );
-//            System.out.println(">>> Artifact created");
-//            System.out.println("You can view or manage this artifact in your browser by accessing:");
-//            System.out.printf("https://console.redhat.com/application-services/service-registry/t/%s/artifacts/default/%s/versions/1\n",
-//                    serviceRegistry.getId(), artifactsMap.get("id"));
-//        } catch (ApiException e) {
-//            if (e.getCode() == 400) {
-//                String msgUnableToExtractParameter =
-//                        "Unable to extract parameter from http request: javax.ws.rs.HeaderParam(\\\"X-Registry-ArtifactType\\\")";
-//                String msgFailedToDiscoverArtifactType = "Failed to discover artifact type from content";
-//                if (e.getMessage().contains(msgUnableToExtractParameter)) {
-//                    System.err.println(">>> " + msgUnableToExtractParameter + " value is '" + artifactType + "'");
-//                    System.err.println(">>> Please specify correct artifact type (XML, AVRO, JSON, etc.)");
-//                } else if (e.getMessage().contains(msgFailedToDiscoverArtifactType)) {
-//                    System.err.println(">>> " + msgFailedToDiscoverArtifactType);
-//                    System.err.println(">>> Please specify artifact type explicitly (--type XML, AVRO, JSON, etc.)");
-//                } else {
-//                    System.err.println(e.getLocalizedMessage());
-//                }
-//            }
-//            return -1;
-//        }
+        apiInstanceClient.setBasePath(API_CLIENT_BASE_PATH);
+        try {
+            // Get Service Registry list
+            Map<String, Object> res = apiInstanceClient.invokeAPI(
+                    SERVICE_REGISTRY_MGMT_URL, "GET", null, null,
+                    new HashMap<>(), new HashMap<>(), new HashMap<>(), ACCEPT_APPLICATION_JSON, APPLICATION_X_WWW_FORM_URLENCODED,
+                    new String[]{"Bearer"}, new GenericType<>() {}
+            );
+
+            if ((res.get("items")) == null || ((ArrayList)res.get("items")).size() == 0) {
+                System.err.println(">>> No Service Registries found!");
+                return -1;
+            } else {
+                // Get first list element
+                LinkedHashMap<String, Object> map = ((ArrayList<LinkedHashMap<String, Object>>)res.get("items")).get(0);
+                // Get registry Url
+                String registryUrl = (String) map.get("registryUrl");
+                apiInstanceClient.setBasePath(registryUrl);
+
+                String url;
+                if (globalId != null) {
+                    url = ARTIFACT_DOWNLOAD_URL + "/globalIds/" + globalId + "/";
+                } else if (contentId != null) {
+                    url = ARTIFACT_DOWNLOAD_URL + "/contentIds/" + contentId + "/";
+                } else {
+                    throw new IllegalArgumentException("Please specify at least one flag: [--content-id or --global-id]!");
+                }
+
+                String response = apiInstanceClient.invokeAPI(
+                        url, "GET", null, null,
+                        new HashMap<>(), new HashMap<>(), new HashMap<>(), ACCEPT_APPLICATION_OCTET_STREAM, CONTENT_TYPE_APPLICATION_JSON,
+                        new String[]{"Bearer"}, new GenericType<>() {}
+                );
+                if (response == null) {
+                    System.err.println(">>> No artifacts found!");
+                    return -1;
+                } else {
+                    if (outputFile == null) {
+                        System.out.println(response);
+                    } else {
+                        String fileName = outputFile;
+                        File file = new File(fileName);
+                        int n = 0;
+                        int count = 0;
+                        while (file.exists()) {
+                            if (++count < 10) {
+                                String fName = fileName + "("+ ++n +")";
+                                file = new File(fName);
+                            } else {
+                                System.err.printf(">>> Too much attempts. Do you really need all these %d files?!\n", count);
+                                System.err.println(">>> File name: " + outputFile);
+                                return -1;
+                            }
+                        }
+                        // write to file
+                        Path artifactFile = file.toPath();
+                        Files.writeString(artifactFile, response, StandardCharsets.UTF_8);
+                        System.out.println(">>> Artifact has been stored in file:\n\t" + file);
+                    }
+                }
+            }
+        } catch (ApiException e) {
+            if (e.getCode() == 404) {
+                System.err.println(">>> No artifacts found!");
+                return -1;
+            } else {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         return 0;
     }
